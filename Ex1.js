1061399337ee30e8ed577d1d52acf65204edb375
Duk Vandeth <dukvandeth657@gmail.com>
2022-02-14 20:24:40 +0700

    TP06

---
+++ Ex1.js
@@ -0,0 +1,83 @@
+const scroller = document.getElementById('scrollable');
+    const loader = document.getElementsByClassName('loading');
+    const menu = document.getElementsByClassName('menu');
+
+    let test = false;
+    let num_page = 0;
+    let crrPageNum = 0;
+    let items = '';
+
+    const loadData = () => {
+        try {
+            fetch(`https://api.instantwebtools.net/v1/passenger?page=${0}&size=${10}`)
+            .then(async (res) => {
+                let passengers = await res.json();
+                let airline = '';
+
+                for (let passenger = 0; passenger <10; passenger++) {
+                    airline = passengers.data[passenger].airline;
+
+                    items += `
+                            <div class="item">
+                                <h4>✈️:${airline[0].name} - ${airline[0].country}</h4>
+                                <p>🙂:${passengers.data[passenger].name}</p>
+                            </div>
+                        `
+                }
+                setTimeout(async() => { //loading spinner
+                    await loading(false);
+                }, 2000);
+
+                if (crrPageNum == 2863) {
+                items += `<div style="text-align: center;">End</div>`
+                    } else {
+                        items += `<hr>`
+                    }
+                    scroller.innerHTML = items;
+                    test = true;
+            });
+        }catch (error) {
+            console.log(error);
+            cur_page--;
+        }
+    };
+    const loading = (load) => { //loading spinner
+        if (load == true) {
+            loader[0].style.display = 'flex';
+            menu[0].style.display = 'none';
+        } else {
+            loader[0].style.display = 'none';
+            menu[0].style.display = 'flex';
+        }
+    }
+    
+    const pageNumber = (num) => {
+        document.getElementsByClassName('page-number')[0].innerHTML = `Page: <span>${num}</span>`;
+    }
+
+    loading(true);
+    loadData();
+
+    scroller.addEventListener('scroll', () => {
+        const clientHeight = scroller.clientHeight;
+        const scollHeight = scroller.scrollHeight;
+        const scrollTop = scroller.scrollTop;
+
+            // console.log('client', clientHeight, ', height', scollHeight, ', Top', scrollTop);
+        if (clientHeight + scrollTop + 200 >= scollHeight && test == true &&  crrPageNum <= 2862) {
+            console.log('loading')
+            loadData();
+            crrPageNum++;
+            test = false;
+        }
+
+        if (clientHeight + scrollTop > (1030 * (num_page + 1)) + 600) {
+            num_page++;
+            pageNumber(num_page);
+            console.log(num_page)
+        } else if (clientHeight + scrollTop <= (1030 * (num_page) + 500)) {
+            num_page--;
+            pageNumber(num_page);
+            console.log(num_page)
+        }
+    })
